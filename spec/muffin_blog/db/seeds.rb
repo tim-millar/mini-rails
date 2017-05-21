617653cd74fe10c425e2c0f3ef9adf7180e6889a
Tim Millar <timothy.millar@onthebeach.co.uk>
2017-05-21 01:41:36 +0100

    Initial project commit

---
+++ spec/muffin_blog/db/seeds.rb
@@ -0,0 +1,13 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+
+Post.create(
+  title: "Blueberry Muffins",
+  body: "These muffins are extra large and yummy with the sugary-cinnamon crumb topping. I usually double the recipe and fill the muffin cups just to the top edge for a wonderful extra-generously-sized deli style muffin. Add extra blueberries too, if you want!"
+)
+
+Post.create(
+  title: "Chocolate Chip Muffins",
+  body: "You won't believe how easily and quickly you can have chocolate chip muffins on the table using this recipe."
+)
